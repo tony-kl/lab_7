d3a15cac4328b3d5a477c1ff7578e1ebc4573764
tony-kl <n01536671@humber.ca>
2023-03-10 17:38:23 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,35 @@
+function easter(){
+    document.getElementById("easter").style.display = 'block';
+    document.body.style.backgroundColor = 'pink';
+    document.body.style.color = 'black';
+}
+
+function halloween(){
+    document.getElementById("halloween").style.display = 'block';
+    document.body.style.backgroundColor = 'orange';
+    document.body.style.color = 'black';
+}
+
+function christmas(){
+    document.getElementById("christmas").style.display = 'block';
+    document.body.style.backgroundColor = 'darkred';
+    document.body.style.color = 'white';
+}
+
+
+function hideEaster(){
+    document.getElementById("easter").style.display = 'none';
+    document.body.style.backgroundColor = 'white';
+    document.body.style.color = 'black';
+}
+function hideHalloween(){
+    document.getElementById("halloween").style.display = 'none';
+    document.body.style.backgroundColor = 'white';
+    document.body.style.color = 'black';
+}
+function hideChristmas(){
+    document.getElementById("christmas").style.display = 'none';
+    document.body.style.backgroundColor = 'white';
+    document.body.style.color = 'black';
+}
+
